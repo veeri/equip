49edc3bfc0ce09d1e52381322e5988e64987d4a4
veeresh <veereshkotravvala333@gmail.com>
2021-01-17 02:22:25 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery/dist/jquery.js';
+import 'bootstrap/dist/js/bootstrap.js';
+import App from './App'
+
+import {BrowserRouter as Router} from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+
+import {store} from "./Redux/store"
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <Router>
+        {/* <PersistGate persistor={persistor}> */}
+          <App />
+        {/* </PersistGate> */}
+      </Router>
+    </Provider>,
   document.getElementById('root')
 );
 
@@ -15,3 +29,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
